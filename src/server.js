1e98e4f5445ce2ba706639caf1f5295779f2ff9e
munawar289 <munawar.rahman289@gmail.com>
2022-04-15 15:16:33 +0700

    restructure with hapi plugin

--- src/server.js
+++ src/server.js
@@ -1,22 +1,51 @@
-/* eslint-disable linebreak-style */
-const Hapi = require('@hapi/hapi');
-const routes = require('./routes');
+// /* eslint-disable linebreak-style */
+// const Hapi = require('@hapi/hapi');
+// const routes = require('./routes');
 
+// const init = async () => {
+//   const server = Hapi.server({
+//     port: 5000,
+//     host: 'localhost',
+//     routes: {
+//         cors: {
+//           origin: ['*'],
+//         },
+//     },
+//   });
+
+//   server.route(routes);
+
+//   await server.start();
+//   console.log(`Server berjalan pada ${server.info.uri}`);
+// };
+
+// init();
+
+const Hapi = require('@hapi/hapi');
+const notes = require('./api/notes');
+const NotesService = require('./services/inMemory/NotesService');
+ 
 const init = async () => {
+  const notesService = new NotesService();
   const server = Hapi.server({
     port: 5000,
-    host: 'localhost',
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
-        cors: {
-          origin: ['*'],
-        },
+      cors: {
+        origin: ['*'],
+      },
     },
   });
-
-  server.route(routes);
-
+ 
+  await server.register({
+    plugin: notes,
+    options: {
+      service: notesService,
+    },
+  });
+ 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
 };
-
+ 
 init();
