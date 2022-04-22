35d2cf3f89918f4bbed92f9066b1ff6d419f2787
munawar289 <munawar.rahman289@gmail.com>
2022-04-22 21:40:33 +0800

    postgres database implemented

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -107,6 +107,163 @@
 //   }
 // }
 
+// const ClientError = require('../../exceptions/ClientError');
+ 
+// class NotesHandler {
+//   constructor(service, validator) {
+//     this._service = service;
+//     this._validator = validator;
+ 
+//     this.postNoteHandler = this.postNoteHandler.bind(this);
+//     this.getNotesHandler = this.getNotesHandler.bind(this);
+//     this.getNoteByIdHandler = this.getNoteByIdHandler.bind(this);
+//     this.putNoteByIdHandler = this.putNoteByIdHandler.bind(this);
+//     this.deleteNoteByIdHandler = this.deleteNoteByIdHandler.bind(this);
+//   }
+ 
+//   postNoteHandler(request, h) {
+//     try {
+//       this._validator.validateNotePayload(request.payload);
+//       const { title = 'untitled', body, tags } = request.payload;
+ 
+//       const noteId = this._service.addNote({ title, body, tags });
+ 
+//       const response = h.response({
+//         status: 'success',
+//         message: 'Catatan berhasil ditambahkan',
+//         data: {
+//           noteId,
+//         },
+//       });
+//       response.code(201);
+//       return response;
+//     } catch (error) {
+//       if (error instanceof ClientError) {
+//         const response = h.response({
+//           status: 'fail',
+//           message: error.message,
+//         });
+//         response.code(error.statusCode);
+//         return response;
+//       }
+ 
+//       // Server ERROR!
+//       const response = h.response({
+//         status: 'error',
+//         message: 'Maaf, terjadi kegagalan pada server kami.',
+//       });
+//       response.code(500);
+//       console.error(error);
+//       return response;
+//     }
+//   }
+ 
+//   getNotesHandler() {
+//     const notes = this._service.getNotes();
+//     return {
+//       status: 'success',
+//       data: {
+//         notes,
+//       },
+//     };
+//   }
+ 
+//   getNoteByIdHandler(request, h) {
+//     try {
+//       const { id } = request.params;
+//       const note = this._service.getNoteById(id);
+//       return {
+//         status: 'success',
+//         data: {
+//           note,
+//         },
+//       };
+//     } catch (error) {
+//       if (error instanceof ClientError) {
+//         const response = h.response({
+//           status: 'fail',
+//           message: error.message,
+//         });
+//         response.code(error.statusCode);
+//         return response;
+//       }
+ 
+//       // Server ERROR!
+//       const response = h.response({
+//         status: 'error',
+//         message: 'Maaf, terjadi kegagalan pada server kami.',
+//       });
+//       response.code(500);
+//       console.error(error);
+//       return response;
+//     }
+//   }
+ 
+//   putNoteByIdHandler(request, h) {
+//     try {
+//       this._validator.validateNotePayload(request.payload);
+//       const { id } = request.params;
+ 
+//       this._service.editNoteById(id, request.payload);
+ 
+//       return {
+//         status: 'success',
+//         message: 'Catatan berhasil diperbarui',
+//       };
+//     } catch (error) {
+//       if (error instanceof ClientError) {
+//         const response = h.response({
+//           status: 'fail',
+//           message: error.message,
+//         });
+//         response.code(error.statusCode);
+//         return response;
+//       }
+ 
+//       // Server ERROR!
+//       const response = h.response({
+//         status: 'error',
+//         message: 'Maaf, terjadi kegagalan pada server kami.',
+//       });
+//       response.code(500);
+//       console.error(error);
+//       return response;
+//     }
+//   }
+ 
+//   deleteNoteByIdHandler(request, h) {
+//     try {
+//       const { id } = request.params;
+//       this._service.deleteNoteById(id);
+ 
+//       return {
+//         status: 'success',
+//         message: 'Catatan berhasil dihapus',
+//       };
+//     } catch (error) {
+//       if (error instanceof ClientError) {
+//         const response = h.response({
+//           status: 'fail',
+//           message: error.message,
+//         });
+//         response.code(error.statusCode);
+//         return response;
+//       }
+ 
+//       // Server ERROR!
+//       const response = h.response({
+//         status: 'error',
+//         message: 'Maaf, terjadi kegagalan pada server kami.',
+//       });
+//       response.code(500);
+//       console.error(error);
+//       return response;
+//     }
+//   }
+// }
+// module.exports = NotesHandler;
+
+// Begin:Tambah Async await
 const ClientError = require('../../exceptions/ClientError');
  
 class NotesHandler {
@@ -121,12 +278,12 @@ class NotesHandler {
     this.deleteNoteByIdHandler = this.deleteNoteByIdHandler.bind(this);
   }
  
-  postNoteHandler(request, h) {
+  async postNoteHandler(request, h) {
     try {
       this._validator.validateNotePayload(request.payload);
       const { title = 'untitled', body, tags } = request.payload;
  
-      const noteId = this._service.addNote({ title, body, tags });
+      const noteId = await this._service.addNote({ title, body, tags });
  
       const response = h.response({
         status: 'success',
@@ -158,8 +315,8 @@ class NotesHandler {
     }
   }
  
-  getNotesHandler() {
-    const notes = this._service.getNotes();
+  async getNotesHandler() {
+    const notes = await this._service.getNotes();
     return {
       status: 'success',
       data: {
@@ -168,10 +325,10 @@ class NotesHandler {
     };
   }
  
-  getNoteByIdHandler(request, h) {
+  async getNoteByIdHandler(request, h) {
     try {
       const { id } = request.params;
-      const note = this._service.getNoteById(id);
+      const note = await this._service.getNoteById(id);
       return {
         status: 'success',
         data: {
@@ -199,12 +356,13 @@ class NotesHandler {
     }
   }
  
-  putNoteByIdHandler(request, h) {
+  async putNoteByIdHandler(request, h) {
     try {
       this._validator.validateNotePayload(request.payload);
+      const { title, body, tags } = request.payload;
       const { id } = request.params;
  
-      this._service.editNoteById(id, request.payload);
+      await this._service.editNoteById(id, { title, body, tags });
  
       return {
         status: 'success',
@@ -231,10 +389,10 @@ class NotesHandler {
     }
   }
  
-  deleteNoteByIdHandler(request, h) {
+  async deleteNoteByIdHandler(request, h) {
     try {
       const { id } = request.params;
-      this._service.deleteNoteById(id);
+      await this._service.deleteNoteById(id);
  
       return {
         status: 'success',
@@ -261,4 +419,7 @@ class NotesHandler {
     }
   }
 }
+ 
 module.exports = NotesHandler;
+
+// END:Tambah Async await
